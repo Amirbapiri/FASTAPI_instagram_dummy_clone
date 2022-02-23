09f586ea6abde9f08ed277aa7db69daf4b9850ff
Amir Bapiri <amirbapiri@gmail.com>
2022-02-23 20:33:56 +0330

    create users table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from db.user import models as user_model
+from db.database import engine
 
 app = FastAPI()
 
@@ -7,3 +9,6 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello dear user :)"
+
+
+user_model.Base.metadata.create_all(bind=engine)
